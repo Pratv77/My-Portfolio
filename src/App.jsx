614be38c302728c5ce6e526d398da4dbb5fe6688
Prat <pratv7@outlook.com>
2024-10-27 21:33:55 -0400

    finished setting up responsive grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,34 @@ import MainProjectCard from "./components/MainProjectCard/MainProjectCard";
 import PhotoGalleryCard from "./components/PhotoGalleryCard/PhotoGalleryCard";
 import ContactCard from "./components/ContactCard/ContactCard";
 import SpotifyCard from "./components/SpotifyCard/SpotifyCard";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div class="max-w-screen-2xl grid auto-rows-[225px] grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-4 p-4">
-      <div class="bg-gray-100 p-4 col-span-2 row-span-2 md:col-span-4">Item 1</div>
-      <div class="bg-gray-100 p-4 col-span-2 md:col-span-4">Item 2</div>
-      <div class="bg-gray-100 p-4 col-span-2 row-span-3 md:col-span-4 md:row-span-2">Item 3</div>
-      <div class="bg-gray-100 p-4 col-span-2 row-span-2">Item 4</div>
-      <div class="bg-gray-100 p-4 col-span-2 row-span-2">Item 5</div>
-      <div class="bg-gray-100 p-4 col-span-2 md:col-span-2">Item 6</div>
-      <div class="bg-gray-100 p-4 col-span-2 md:col-span-2">Item 7</div>
+    <div className="min-h-screen p-8 grid place-items-center">
+      <div class="w-full max-w-screen-2xl grid auto-rows-[225px] grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-4">
+        <div class="col-span-2 row-span-2 md:col-span-4">
+          <Card>Item 1</Card>
+        </div>
+        <div class="col-span-2 md:col-span-4">
+          <Card>Item 2</Card>
+        </div>
+        <div class="col-span-2 row-span-3 md:col-span-4 md:row-span-2">
+          <Card>Item 3</Card>
+        </div>
+        <div class="col-span-2 row-span-2">
+          <Card>Item 4</Card>
+        </div>
+        <div class="col-span-2 row-span-2">
+          <Card>Item 5</Card>
+        </div>
+        <div class="col-span-2 md:col-span-2">
+          <Card>Item 6</Card>
+        </div>
+        <div class="col-span-2 md:col-span-2">
+          <Card>Item 7</Card>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = ({ children }) => {
+  return (
+    <div className="min-h-full bg-gray-50/[.02] hover:bg-gray-50/[.04] transition duration-300 rounded-lg border border-white/25">
+      {children}
+    </div>
+  );
+};
+
+export default Card;
