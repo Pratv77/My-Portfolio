745234d12c9d31b79d117bf85d4684d130419eed
Prat <pratv7@outlook.com>
2024-10-29 02:15:57 -0400

    added WIP

--- src/components/MainProjectCard/MainProjectCard.jsx
+++ src/components/MainProjectCard/MainProjectCard.jsx
@@ -3,7 +3,8 @@ import "../../App.css";
 
 const MainProjectCard = () => {
   return (
-    <div className="animated-background h-full w-full rounded-lg bg-gradient-to-r from-blue-500 via-blue-500 to-indigo-500">
+    <div className="animated-background h-full w-full rounded-lg bg-gradient-to-r from-blue-500 via-blue-500 to-indigo-500 grid place-content-center">
+      <p className="font-cabin-sketch text-white opacity-70 text-6xl text-center">Work-In-Progress</p>
     </div>
   );
 };
