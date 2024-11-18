4b0bd68ae58ae8d41916979d9b792dacd1cd16b7
Prat <pratv7@outlook.com>
2024-11-18 18:03:32 -0500

    testing deployment

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist', // Default build directory
-    assetsDir: 'assets', // Ensure static assets like JS and CSS are under this folder
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
