b6fc1767da4ea53abc2e260084440e4dbe0bcc5f
Prat <pratv7@outlook.com>
2024-11-21 01:01:28 -0500

    contact hover fixed

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -6,9 +6,9 @@ const ContactCard = ({ onClick }) => {
     <div
       onClick={onClick}
       style={{ backgroundImage: `url(${background})` }}
-      className="rounded-lg h-full grid place-content-center group cursor-pointer border border-white/25 transition-all ease-in-out"
+      className="rounded-lg h-full grid place-content-center group cursor-pointer border border-white/25 transition-all duration-300 ease-in-out"
     >
-      <p className="font-cabin-sketch w-[300px] text-gray-400 text-5xl sm:text-7xl lg:text-4xl lg:w-[200px] xl:text-6xl xl:w-[300px] text-center group-hover:scale-[1.02] group-hover:text-gray-50">
+      <p className="font-cabin-sketch w-[300px] text-gray-400 text-5xl sm:text-7xl lg:text-4xl lg:w-[200px] xl:text-6xl xl:w-[300px] text-center transform transition-transform transition-colors duration-300 ease-in-out group-hover:scale-[1.02] group-hover:text-gray-50">
         Leave a Message!
       </p>
     </div>
